0baf3ea20e2c1a244b766a3e64e3a2faeeaa624c
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2024-04-27 08:25:28 +0300

    add dynamic color

---
+++ app/src/main/java/com/mostafadevo/noteluxe/Application.kt
@@ -0,0 +1,11 @@
+package com.mostafadevo.noteluxe
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
