c3f92d0afa106c4d3b0d74c198b7aa9dc7257c31
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2024-04-29 00:24:22 +0300

    import LinearLayoutManager

--- app/src/main/java/com/mostafadevo/noteluxe/fragments/list/ListFragment.kt
+++ app/src/main/java/com/mostafadevo/noteluxe/fragments/list/ListFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.mostafadevo.noteluxe.R
 import com.mostafadevo.noteluxe.databinding.FragmentListBinding
 
