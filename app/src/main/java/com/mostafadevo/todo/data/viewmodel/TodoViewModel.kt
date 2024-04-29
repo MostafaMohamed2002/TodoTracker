048461c454a21f9db72e0ccdde0154a031237d90
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2024-04-29 10:14:17 +0300

    add: insert todo method

--- app/src/main/java/com/mostafadevo/todo/data/viewmodel/TodoViewModel.kt
+++ app/src/main/java/com/mostafadevo/todo/data/viewmodel/TodoViewModel.kt
@@ -1,11 +1,13 @@
-package com.mostafadevo.todo.fragments.list
+package com.mostafadevo.todo.data.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import com.mostafadevo.todo.data.TodoDataBase
 import com.mostafadevo.todo.data.model.Todo
 import com.mostafadevo.todo.data.repo.TodoRepository
+import kotlinx.coroutines.launch
 
 class TodoViewModel(application: Application) : AndroidViewModel(application) {
     private val repository: TodoRepository
@@ -15,5 +17,10 @@ class TodoViewModel(application: Application) : AndroidViewModel(application) {
     init {
         repository = TodoRepository(todoDAO)
         getAllTodo = repository.getAllTodo
+    }
+    fun insertTodo(todo: Todo){
+        viewModelScope.launch {
+            repository.insertTodo(todo)
+        }
     }
 }
