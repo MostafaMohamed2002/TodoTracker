b69657075955f1eeb0379bd6f56a643ff6d036f9
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2024-04-29 14:02:11 +0300

    add : viewModel Factory

--- app/src/main/java/com/mostafadevo/todo/data/viewmodel/TodoViewModel.kt
+++ app/src/main/java/com/mostafadevo/todo/data/viewmodel/TodoViewModel.kt
@@ -3,13 +3,25 @@ package com.mostafadevo.todo.data.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.mostafadevo.todo.data.TodoDataBase
 import com.mostafadevo.todo.data.model.Todo
 import com.mostafadevo.todo.data.repo.TodoRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class TodoViewModel(application: Application) : AndroidViewModel(application) {
+    class TodoViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(TodoViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return TodoViewModel(application) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
     private val repository: TodoRepository
     private val todoDAO = TodoDataBase.getDatabase(application).todoDao()
     private val getAllTodo: LiveData<List<Todo>>
@@ -19,7 +31,7 @@ class TodoViewModel(application: Application) : AndroidViewModel(application) {
         getAllTodo = repository.getAllTodo
     }
     fun insertTodo(todo: Todo){
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             repository.insertTodo(todo)
         }
     }
