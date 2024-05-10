11ac76f2f5efbfb4b79ca7b8e52ff578620e3c57
mostafadevo <Mostafa0Mohamed2002@gmail.com>
2024-05-10 16:42:49 +0300

    feat: add upload todos to firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.annotation)
+    implementation(libs.firebase.database.ktx)
     val room_version = "2.6.1"
 
     implementation("androidx.room:room-runtime:$room_version")
--- app/src/main/java/com/mostafadevo/todo/data/repo/TodoRepository.kt
+++ app/src/main/java/com/mostafadevo/todo/data/repo/TodoRepository.kt
@@ -1,24 +1,41 @@
 package com.mostafadevo.todo.data.repo
 
 import androidx.lifecycle.LiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.mostafadevo.todo.data.TodoDAO
 import com.mostafadevo.todo.data.model.Todo
 
 class TodoRepository(private val todoDAO: TodoDAO) {
-
+    private val TAG = "TodoRepository"
+    private val currentUser = FirebaseAuth.getInstance().currentUser?.uid
     val getAllTodo: LiveData<List<Todo>> = todoDAO.getAllTodos()
+    private val fireStoreDB = FirebaseFirestore.getInstance()
+        .collection("users")
+        .document(currentUser.toString())
+        .collection("todos")
+
 
     suspend fun insertTodo(todo: Todo) {
         todoDAO.insertTodo(todo)
-    }
-    suspend fun deleteAllTodos() {
-        todoDAO.deleteAllTodos()
-    }
-    suspend fun updateTodo(todo: Todo){
-        todoDAO.updateTodo(todo)
+        fireStoreDB.document(todo.id).set(todo)
     }
 
-     fun getTodoSortedBy(sortType: String): LiveData<List<Todo>> {
+    suspend fun deleteAllTodos() {
+        todoDAO.deleteAllTodos()
+        fireStoreDB.get().addOnSuccessListener { querySnapshot ->
+            querySnapshot.documents.forEach {
+                fireStoreDB.document(it.id).delete()
+            }
+        }
+    }
+
+    suspend fun updateTodo(todo: Todo) {
+        todoDAO.updateTodo(todo)
+        fireStoreDB.document(todo.id).set(todo)
+    }
+
+    fun getTodoSortedBy(sortType: String): LiveData<List<Todo>> {
         return when (sortType) {
             "newest" -> todoDAO.sortByNewest()
             "oldest" -> todoDAO.sortByOldest()
@@ -32,8 +49,10 @@ class TodoRepository(private val todoDAO: TodoDAO) {
 
     suspend fun deleteTodoItem(itemToDelete: Todo) {
         todoDAO.deleteTodoItem(itemToDelete)
+        fireStoreDB.document(itemToDelete.id).delete()
     }
-    suspend fun search(query:String) : LiveData<List<Todo>> {
+
+    suspend fun search(query: String): LiveData<List<Todo>> {
         return todoDAO.search(query)
     }
 }
